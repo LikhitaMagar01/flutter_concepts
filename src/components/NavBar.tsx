b3c43a385aa3ab9e5a644758e181e245b94d2e49
LikhitaMagar01 <leekhitame@gmail.com>
2023-12-22 10:33:44 +0545

    style: nav content

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,45 +1,47 @@
-"use client"
-import { BiSolidBox } from 'react-icons/bi'
-import { PiHamburgerLight } from 'react-icons/pi'
-import { AiOutlineArrowDown } from 'react-icons/ai'
-import Link from 'next/link'
-import { useState } from 'react'
-import Drawer from './Drawer'
+"use client";
+import { BiSolidBox } from "react-icons/bi";
+import { PiHamburgerLight } from "react-icons/pi";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import Link from "next/link";
+import { useState } from "react";
+import Drawer from "./Drawer";
 
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    function toggle() {
-        setIsOpen((isOpen) => !isOpen)
-    }
-    return <nav className='flex justify-between md:justify-around p-5'>
-        <div>
-            <BiSolidBox size={25} href={"/"} className="cursor-pointer" />
+  const [isOpen, setIsOpen] = useState(false);
+  function toggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <nav className="md:flex block justify-between md:justify-around p-5">
+      <div className="flex gap-x-6 justify-between">
+        <div>SiLai</div>
+        <div className="flex gap-x-2">
+          <PiHamburgerLight
+            size={25}
+            onClick={toggle}
+            className="cursor-pointer flex"
+          />
+          <div className="md:block hidden">Categories</div>
         </div>
-        <PiHamburgerLight size={25} onClick={toggle} className='cursor-pointer md:hidden flex' />
-        {
-            isOpen && (<Drawer isOpen={isOpen} onClose={toggle}>
-                <div className='flex flex-col gap-4 p-2'>
-                    <Link href="/sign-in" className='cursor-pointer'>Sign In</Link>
-                    <Link href="/sign-up" className='flex gap-x-1 outline outline-offset-2 outline-1 cursor-pointer'><div>Sign Up</div><span className='animate-bounce'><AiOutlineArrowDown size={25} /></span></Link>
-                    <Link href={'/'}>Home</Link>
-                    <Link href={'/game'}>Game</Link>
-                    <Link href={'/rules'}>How to play</Link>
-                    <Link href={'/highest-score'}>Top Scorer</Link>
-                </div>
-            </Drawer>
-            )
-        }
-        <div className='gap-x-8 hidden md:flex'>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/game'}>Game</Link>
-            <Link href={'/rules'}>How to play</Link>
-            <Link href={'/highest-score'}>Top Scorer</Link>
-        </div>
-        <div className='gap-x-4 hidden md:flex'>
-            <Link href="/sign-in" className='cursor-pointer'>Sign In</Link>
-            <Link href="/sign-up" className='flex items-center px-1 py-0 rounded gap-x-1 outline outline-offset-2 outline-1 cursor-pointer'><div>Sign Up</div><span className='animate-bounce'><AiOutlineArrowDown size={16} /></span></Link>
-        </div>
-    </nav>
-}
+      </div>
 
-export default NavBar
+      {isOpen && (
+        <Drawer isOpen={isOpen} onClose={toggle}>
+          <div className="flex flex-col gap-4 p-2">
+            Category one Category two
+          </div>
+        </Drawer>
+      )}
+      <div className="gap-x-8 hidden md:flex">search field</div>
+      <div className="gap-x-4 hidden md:flex">
+        <Link href="/sign-in" className="cursor-pointer">
+          Sign In
+        </Link>
+        <BiSolidBox size={25} href={"/"} className="cursor-pointer" />
+        <AiOutlineArrowDown size={25} href={"/"} className="cursor-pointer" />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
